c756a64343c826588f7b0fa3d845a16d019902b8
Levi Knight <levikknight@gmail.com>
2021-09-16 17:04:50 -0500

    Done with 02.2:1-3.

--- app/src/main/java/com/example/twoactivitiesattempttwo/SecoundActivity.java
+++ app/src/main/java/com/example/twoactivitiesattempttwo/SecoundActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecoundActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = SecoundActivity.class.getSimpleName();
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
     private EditText mReply;
 
@@ -29,6 +31,7 @@ public class SecoundActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
